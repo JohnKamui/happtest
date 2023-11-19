1951da996acd624ae9522d29dcdaa76fa8741474
JohnKamui <108286181+JohnKamui@users.noreply.github.com>
2023-11-19 19:45:59 -0300

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,12 @@ const Navbar = async () => {
       <div className='container max-w-7xl h-full mx-auto flex items-center justify-between gap-2'>
         {/* logo */}
         <Link href='/' className='flex gap-2 items-center'>
-          <Icons.logo className='h-8 w-8 sm:h-6 sm:w-6' />
+         <img
+             src="https://media.discordapp.net/attachments/1175489244165312593/1175812393763356806/gggghgjh.png?ex=656c97a0&is=655a22a0&hm=e9990498057ffa88dc57c69e22e27e2d9e3e5f0cb17e245ac091cf82e8b54396&="
+             alt="Descrição da Imagem"
+             style="width: 80px; height: 60px;"
+         />
+
           <p className='hidden text-zinc-700 text-sm font-medium md:block'>NuKe</p>
         </Link>
 
