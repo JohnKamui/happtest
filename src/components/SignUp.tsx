411a47daa9ddf17a6ad129c8ed31db9b3b096e36
JohnKamui <108286181+JohnKamui@users.noreply.github.com>
2023-11-19 18:48:27 -0300

    Update SignUp.tsx

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -7,19 +7,19 @@ const SignUp = () => {
     <div className='container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]'>
       <div className='flex flex-col space-y-2 text-center'>
         <Icons.logo className='mx-auto h-6 w-6' />
-        <h1 className='text-2xl font-semibold tracking-tight'>Sign Up</h1>
+    <h1 className='text-2xl font-semibold tracking-tight'>Registre-Se Aqui</h1>
         <p className='text-sm max-w-xs mx-auto'>
-          By continuing, you are setting up a Breadit account and agree to our
-          User Agreement and Privacy Policy.
+          Continuando, Você Está prestes a configurar uma Conta No Happ
+          e Aceitar Nossos Termos De Condição.
         </p>
       </div>
       <UserAuthForm />
       <p className='px-8 text-center text-sm text-muted-foreground'>
-        Already a Breadditor?{' '}
+        Já é um Usuario?{' '}
         <Link
           href='/sign-in'
           className='hover:text-brand text-sm underline underline-offset-4'>
-          Sign in
+          Entrar
         </Link>
       </p>
     </div>
