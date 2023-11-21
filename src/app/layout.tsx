9617779f2b0fdeb0cb5ea51a5ab37c8302a3b755
JohnKamui <108286181+JohnKamui@users.noreply.github.com>
2023-11-20 21:45:13 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     <html
       lang='en'
       className={cn(
-        'bg-black text-slate-900 antialiased light',
+        'bg-white text-slate-900 antialiased light',
         inter.className
       )}>
       <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
